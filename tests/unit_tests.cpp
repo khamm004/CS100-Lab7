b2ff72521ffcf9daf9b204b1fb5cb9d5e3d6b5c0
glee139 <glee00328@gmail.com>
2021-03-01 17:31:37 -0800

    updated unit tests
    * added tests to demonstrate individual object functionality and nested functionality
    * deleted factory and base pointer in each test to resolve valgrind issues
    * added tests to notify when invalid input is used

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -9,9 +9,10 @@ TEST(FACTORY, MULT_OPS){
         char* test_val[6] = {"","3","+","7","*","2"};
         Factory* f =new Factory();
         Base* test = f->parse(test_val, 6);
+	EXPECT_EQ(20, test->evaluate());
         EXPECT_EQ("((3.000000+7.000000)*2.000000)",test->stringify());
-        EXPECT_EQ(20, test->evaluate());
 	delete f;
+	delete test;
 }
 
 TEST(FACTORY, ALL_OPS){
@@ -21,6 +22,7 @@ TEST(FACTORY, ALL_OPS){
         EXPECT_EQ("(((((3.000000+7.000000)*2.000000)/1.000000)**2.000000)-1.000000)",test->stringify());
         EXPECT_EQ(399, test->evaluate());
 	delete f;
+        delete test;
 }
 
 
@@ -31,8 +33,96 @@ TEST(FACTORY, EX){
         EXPECT_EQ("(3.000000+7.000000)",test->stringify());
         EXPECT_EQ(10, test->evaluate());
 	delete f;
+        delete test;
+}
+TEST(FACTORY, LETTERS){
+        char* test_val[6] = {"","a","+","b","*","2"};
+        Factory* f =new Factory();
+        EXPECT_EQ(nullptr, f->parse(test_val, 6));
+	delete f;
 }
 
+TEST(FACTORY, OPERATOR_FIRST){
+        char* test_val[5] = {"","+","7","*","9"};
+        Factory* f =new Factory();
+        EXPECT_EQ(nullptr, f->parse(test_val, 5));
+	delete f;
+}
+
+TEST(FACTORY, ADD){
+        char *test_val[4] = {"", "3", "+", "7"};
+        Factory* f =new Factory();
+        Base* test = f->parse(test_val, 4);
+        EXPECT_EQ("(3.000000+7.000000)",test->stringify());
+        EXPECT_EQ(10, test->evaluate());
+	delete f;
+        delete test;
+}
+
+TEST(FACTORY, SUB){
+        char *test_val[4] = {"", "7", "-", "3"};
+        Factory* f =new Factory();
+        Base* test = f->parse(test_val, 4);
+        EXPECT_EQ("(7.000000-3.000000)",test->stringify());
+        EXPECT_EQ(4, test->evaluate());
+	delete f;
+        delete test;
+}
+
+TEST(FACTORY, MULT){
+        char *test_val[4] = {"", "7", "*", "3"};
+        Factory* f =new Factory();
+        Base* test = f->parse(test_val, 4);
+        EXPECT_EQ("(7.000000*3.000000)",test->stringify());
+        EXPECT_EQ(21, test->evaluate());
+	delete f;
+        delete test;
+}
+
+TEST(FACTORY, DIV){
+        char *test_val[4] = {"", "15", "/", "3"};
+        Factory* f =new Factory();
+        Base* test = f->parse(test_val, 4);
+        EXPECT_EQ("(15.000000/3.000000)",test->stringify());
+        EXPECT_EQ(5, test->evaluate());
+	delete f;
+        delete test;
+}
+
+TEST(FACTORY, POW){
+        char *test_val[4] = {"", "2", "**", "3"};
+        Factory* f =new Factory();
+        Base* test = f->parse(test_val, 4);
+        EXPECT_EQ("(2.000000**3.000000)",test->stringify());
+        EXPECT_EQ(8, test->evaluate());
+	delete f;
+        delete test;
+}
+
+TEST(FACTORY, ONLY_ONE_NUM){
+        char* test_val[2] = {"","9"};
+        Factory* f =new Factory();
+	Base* test = f->parse(test_val, 2);
+        EXPECT_EQ("9.000000",test->stringify());
+        EXPECT_EQ(9, test->evaluate());
+	delete f;
+        delete test;
+}
+TEST(FACTORY, MULTI_DIGIT){
+        char *test_val[6] = {"", "100", "*", "3","/","20"};
+        Factory* f =new Factory();
+        Base* test = f->parse(test_val, 6);
+        EXPECT_EQ("((100.000000*3.000000)/20.000000)",test->stringify());
+        EXPECT_EQ(15, test->evaluate());
+	delete f;
+        delete test;
+}
+TEST(FACTORY, OPERATOR_LAST){
+        char* test_val[5] = {"","7","*","9","+"};
+        Factory* f =new Factory();
+        EXPECT_EQ(nullptr, f->parse(test_val, 5));
+	delete f;
+}
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
